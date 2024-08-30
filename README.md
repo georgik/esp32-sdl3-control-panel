95aa4e22274ccccf330126eebd6957fccdc38c3c
Juraj Michalek <juraj.michalek@gmail.com>
2024-08-30 08:00:25 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Test of port of SDL3 for ESP32
+
+Experimental port of SDL3 to ESP32 ESP-IDF v5.3.
+
+Working parts:
+- minimalistic SDL3 build
+- SDL3 - littlefs integration
+- SDL3\_timer
+- SDL\_image - BMP
+- SDL\_ttf
+
+## Requirements
+
+`idf_component_manager` 2.x - install manually
+
+## Build
+
+```
+git clone git@github.com:georgik/esp32-sdl3-test.git
+cd esp32-sdl3-test
+
+idf.py @boards/esp-box-3.cfg set-target esp32-s3
+idf.py @boards/esp-box-3.cfg build
+```
+
+### Other boards
+
+- ESP32-S3-BOX-3
+```shell
+idf.py @boards/esp-box-3.cfg reconfigure
+```
+
+- ESP32-S3-BOX (prior Dec. 2023)
+```shell
+idf.py @boards/esp-box.cfg reconfigure
+```
+
+- ESP32-P4
+```shell
+idf.py @boards/esp32_p4_function_ev_board.cfg reconfigure
+```
+
+- M5Stack-CoreS3
+```shell
+idf.py @boards/m5stack_core_s3.cfg reconfigure
+```
+
+## Notes
+
+- screen resolution bigger than 320x100 requires enabled PSRAM
+- SDL surface for ESP-IDF framebuffer is defined for RGB565
+- drawing surface requires `SDL_FRect` as destination, not `SDL_Rect`, in `SDL_Rect` case nothing draws
+
+## Credits
+
+- FreeSans.ttf - https://github.com/opensourcedesign/fonts/blob/master/gnu-freefont_freesans/FreeSans.ttf
---
+++ main/sdl-test.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include "SDL3/SDL.h"
+#include "SDL3_image/SDL_image.h"
+#include "SDL3_ttf/SDL_ttf.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "graphics.h"
+#include "filesystem.h"
+#include "text.h"
+
+
+Uint64 TimerCallback(void *param, Uint64 interval)
+{
+    // printf("Timer callback executed!\n");
+    return interval; // Return the interval to keep the timer running
+}
+
+void app_main(void) {
+    printf("SDL3 on ESP32\n");
+
+    if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO) != 0) {
+        printf("Unable to initialize SDL: %s\n", SDL_GetError());
+        return;
+    }
+    printf("SDL initialized successfully\n");
+
+    SDL_Window *window = SDL_CreateWindow("SDL on ESP32", 320, 240, 0);
+    if (!window) {
+        printf("Failed to create window: %s\n", SDL_GetError());
+        return;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+    if (!renderer) {
+        printf("Failed to create renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        return;
+    }
+
+    clear_screen(renderer);
+
+    SDL_InitFS();
+    // TestFileOpen("/assets/espressif.bmp");
+
+    TTF_Font *font = initialize_font("/assets/FreeSans.ttf", 12);
+    if (!font) return;
+
+    // Create a repeating timer with a 1-second interval
+    SDL_TimerID timer_id = SDL_AddTimer(1000, TimerCallback, NULL);
+    if (timer_id == 0) {
+        printf("Failed to create timer: %s\n", SDL_GetError());
+    } else {
+        printf("Timer created successfully\n");
+    }
+
+#ifndef CONFIG_IDF_TARGET_ESP32P4
+    SDL_Texture *textTexture = render_text(renderer, font, "Tomik", (SDL_Color){255, 255, 255, 255});
+#endif
+    SDL_Texture *dashboard_texture = LoadBackgroundImage(renderer, "/assets/screen-dashboard.bmp");
+    SDL_Texture *controls_texture = LoadBackgroundImage(renderer, "/assets/screen-controls.bmp");
+    SDL_Texture *apps_texture = LoadBackgroundImage(renderer, "/assets/screen-apps.bmp");
+    SDL_Texture *settings_texture = LoadBackgroundImage(renderer, "/assets/screen-settings.bmp");
+    SDL_Texture *thermostat_texture = LoadBackgroundImage(renderer, "/assets/screen-thermostat.bmp");
+
+    // Animation variables
+    float rect_x = 10.0f, speed = 2.0f;
+    int direction = 1;
+
+    // Variables for the BMP position and speed
+    float bmp_x = 2.0f;
+    float bmp_y = 2.0f;
+    float bmp_speed_x = 2.0f;
+    float bmp_speed_y = 2.0f;
+
+   // Variables for the text position, speed, and size
+    float text_x = 30.0f, text_y = 40.0f;
+    float text_speed_x = 1.5f, text_speed_y = 1.2f;
+    float text_scale = 1.0f, text_scale_speed = 0.01f;
+    int text_direction_x = 1, text_direction_y = 1;
+    int scale_direction = 1;
+
+    while (1) {
+        // Move the BMP image
+        bmp_x += bmp_speed_x;
+        bmp_y += bmp_speed_y;
+
+        // Check for collisions with screen edges and bounce
+        if (bmp_x <= 0 || bmp_x + 32 >= 320) bmp_speed_x *= -1;
+        if (bmp_y <= 0 || bmp_y + 32 >= 240) bmp_speed_y *= -1;
+
+        // Move the rectangle and bounce it
+        rect_x += speed * direction;
+
+        if (rect_x <= 0 || rect_x + 50 >= 320) direction *= -1;
+
+       // Move the text and bounce it
+        text_x += text_speed_x * text_direction_x;
+        text_y += text_speed_y * text_direction_y;
+        if (text_x <= 0 || text_x >= 200) text_direction_x *= -1;
+        if (text_y <= 0 || text_y >= 200) text_direction_y *= -1;
+
+        // Scale the text size
+        text_scale += text_scale_speed * scale_direction;
+        if (text_scale <= 0.5f || text_scale >= 2.0f) scale_direction *= -1;
+
+        // Clear screen and draw
+        clear_screen(renderer);
+        draw_image(renderer, dashboard_texture, 0, 0, 320.0f, 240.0f);
+        // draw_moving_rectangles(renderer, rect_x);
+#ifndef CONFIG_IDF_TARGET_ESP32P4
+        draw_text(renderer, textTexture, text_x, text_y, 120, 20 * text_scale);
+#endif
+
+        SDL_RenderPresent(renderer);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+        draw_image(renderer, controls_texture, 0, 0, 320.0f, 240.0f);
+        SDL_RenderPresent(renderer);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+        draw_image(renderer, apps_texture, 0, 0, 320.0f, 240.0f);
+        SDL_RenderPresent(renderer);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+        draw_image(renderer, settings_texture, 0, 0, 320.0f, 240.0f);
+        SDL_RenderPresent(renderer);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+        draw_image(renderer, thermostat_texture, 0, 0, 320.0f, 240.0f);
+        SDL_RenderPresent(renderer);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+    }
+}
